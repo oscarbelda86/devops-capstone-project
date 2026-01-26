f014b3c2906eb84cc1de78c5fe4297b93252d545
oscarbelda86 <oscarbelda86@gmail.com>
2026-01-25 22:10:24 -0300

    Add docstrings to account-related endpoints

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
+
 def list_accounts():
     """
     List all Accounts
@@ -77,6 +78,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
 def get_accounts(account_id):
     """
     Reads an Account
@@ -93,6 +95,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
+
 def update_accounts(account_id):
     """
     Update an Account
@@ -112,6 +115,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
 def delete_accounts(account_id):
     """
     Delete an Account
