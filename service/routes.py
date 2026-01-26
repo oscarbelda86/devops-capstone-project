58d681bc1a4facb4e6407f145b63d5bc87d13dec
oscarbelda86 <oscarbelda86@gmail.com>
2026-01-25 22:22:36 -0300

    Clean up blank lines in account routes
    Removed unnecessary blank lines in routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,9 +61,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
-
 def list_accounts():
     """
     List all Accounts
@@ -81,9 +79,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -101,9 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-
 def update_accounts(account_id):
     """
     Update an Account
@@ -124,9 +118,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
 def delete_accounts(account_id):
     """
     Delete an Account
